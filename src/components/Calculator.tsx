74b278ea8abde2c01669e3a54b13610b6008c8ad
NoobCodeAjay <ajay.jadhav@humancloud.co.in>
2025-01-17 16:51:08 +0530

    feat: implement calculator buttons and display to pass functionality test case

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,5 +1,29 @@
 const CalculatorUI = () => {
-  return <div data-testid="calculator">Calculaor</div>;
+  return (
+    <div data-testid="calculator">
+      <div data-testid="display">0</div>
+      <div>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <button key={index} name={index.toString()} className="number-button">
+            {index}
+          </button>
+        ))}
+
+        {["+", "-", "×"].map((operator) => (
+          <button key={operator} name={operator} className="operator-button">
+            {operator}
+          </button>
+        ))}
+
+        <button name="=" className="equals-button">
+          =
+        </button>
+        <button name="C" className="clear-button">
+          C
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default CalculatorUI;
